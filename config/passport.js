cd6e98af49e529c76c610f0a262f0c51e291b9e1
dhilsheraf <dhilsherafummer@gmail.com>
2024-12-29 14:46:11 +0530

    uri s check

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.NODE_ENV === 'production'
-    ? 'https://tromic.shop/auth/google/callback'
+    ? 'http://tromic.shop/auth/google/callback'
     : 'http://localhost:3000/auth/google/callback'
 },
 
